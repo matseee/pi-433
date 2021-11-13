1e0b63f636379cd3826a0d025ea8598795b58333
Matthias Fischer <m@tthias.space>
2021-11-13 14:50:04 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# Copy wrapper to the init directory
-cp ./Pi433Daemon.conf /etc/init/Pi433Daemon.conf
-
 # Read out the current path
 PWD=$(pwd)
 
 # Change the path in the config file
-sed "s|___PATH___|${PWD}|g" /etc/init/Pi433Daemon.conf > /etc/init/Pi433Daemon.conf
+sed "s|___PATH___|${PWD}|g" ./Pi433Daemon.conf > /etc/init/Pi433Daemon.conf
 
 # reboot the system
-# reboot
+reboot
