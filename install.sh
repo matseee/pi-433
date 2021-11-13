e2993b2a5b9ec6aafad4989625f281a5a8021b52
Matthias Fischer <m@tthias.space>
2021-11-13 14:42:32 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ PWD=$(pwd)
 # Change the path in the config file
 sed -i -e "s/___PATH___/${PWD}/g" /etc/init/Pi433Daemon.conf
 
-# Check the config
-init-checkconf /etc/init/Pi433Daemon.conf
+# reboot the system
+# reboot
